2871ab31a39d2ab1121adc50b0b3cd3d61588b78
Pai Han <expertdev811@outlook.com>
2018-03-20 10:34:25 +0900

    update widget2

--- widget2/index.php
+++ widget2/index.php
@@ -1,4 +1,15 @@
 <?php
-$url = $_GET['url'];
+if(!empty($_GET['url'])) {
+	$url = "https://helpdesk.bitrix24.com";
+	$p = 0;
+	foreach ($_GET as $key=>$value){
+		if($p == 0) {
+			$url .=  "/?".$key."=".urldecode($value);
+		}else {
+			$url .= "&".$key."=".urldecode($value);
+		}
+		$p++;
+	}
+} 
 header('Location: http://helpdesk.thurlyos.com/?url='.$url);
 ?>
