d5fc27c8967511347ae2ed5e4b03beb2caf2167e
bluebear925 <bluebear925@gmail.com>
2018-03-22 20:28:30 +0900

    error report

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include("simple_html_dom.php");
 $url = "https://helpdesk.bitrix24.com/widget2/";
 
